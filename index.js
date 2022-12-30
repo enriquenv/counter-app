73ca5ddf3e83a8b518dccfda0d11aed6a85dc0f6
enriquenv <enriquen@live.de>
2022-12-29 23:16:54 -0500

    minimalist counter app

---
+++ index.js
@@ -0,0 +1,51 @@
+let teilnehmer = document.getElementById("teilnehmer");
+let count = 0;
+
+function add() {
+    count = count + 1;
+    teilnehmer.innerText = count;
+}
+
+let savedCount = document.getElementById("saved-count");
+
+if (typeof localStorage !== 'undefined') {
+    // get values in local storage when enabled
+    let saved = Number(localStorage.getItem('saved'));
+    savedCount.innerText = saved
+
+    function save() {
+    saved = Number(saved) + count;
+    localStorage.setItem('saved', saved);
+    savedCount.innerText = saved;
+
+    count = 0;
+    teilnehmer.innerText = count;
+    }
+
+    function neu() {
+        localStorage.removeItem('saved');
+        savedCount.innerText = 0;
+        count = 0;
+        teilnehmer.innerText = count;
+        saved = 0;
+    }
+
+  } else {
+    // if local storage is not supported or disabled
+        let saved = 0;
+        savedCount.innerText = saved;
+
+        function save() {
+            saved = saved + count;
+            savedCount.innerText = saved;
+        
+            count = 0;
+            teilnehmer.innerText = count;
+            }
+
+        function neu() {
+            location.reload();
+        }
+
+  }
+    
